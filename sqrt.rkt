1cec1268e956d9fa91ec5905e34715528ae17be7
Tiago Katcipis <tiagokatcipis@gmail.com>
2018-02-09 22:55:50 -0200

    Properly calculate a sqrt

--- sqrt.rkt
+++ sqrt.rkt
@@ -2,29 +2,15 @@
 
 (require "damp.rkt" "fixed-point.rkt")
 
-
-(define (sqrt-base x next guess precision maxiter)
-  (fixed-point
-    (lambda (guess)
-      (+
-        guess
-        (abs (- (* guess guess) x))
-        ))
-    next
-    guess
-    precision
-    maxiter
-    ))
-
 (define (sqrt-heron x)
-  (sqrt-base
-    x
-    (lambda (guess) (avgdamp
-                      (lambda (guess) (/ x guess))
-                      guess
-                      ))
-    1
-    0.00001
-    1000))
+    (define start 1)
+    (define precision 0.000001)
+    (define maxiter 10000)
+
+    (fixed-point
+      (avgdamp (lambda (guess) (/ x guess)))
+      start
+      precision
+      maxiter))
 
 (provide sqrt-heron)
